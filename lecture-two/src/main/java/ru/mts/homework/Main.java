d4507f97db77d0c448bece9782973e90bce4d0d6
AlinaFedMTS <alin.dolgova2013@yandex.ru>
2024-02-20 23:57:18 +0300

    Test diff 2

--- lecture-two/src/main/java/ru/mts/homework/Main.java
+++ lecture-two/src/main/java/ru/mts/homework/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!Hello world!Hello world!Hello world!Hello world!");
         System.out.println("Test");
-        System.out.println("Test2");
+        System.out.println("Test3");
     }
 }
